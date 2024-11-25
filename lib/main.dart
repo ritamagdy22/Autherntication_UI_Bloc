2d879f0ff6abeb922670726fa526c9eb6ed18651
ritamagdy22 <121579272+ritamagdy22@users.noreply.github.com>
2024-11-25 03:48:31 +0200

    Updating UI only

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:api/cubit/User_Cubit.dart';
+import 'package:api/screens/sign_in_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+void main() {
+  runApp(
+    BlocProvider(
+      create: (context) => UserCubit(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignInScreen(),
+    );
+  }
+}
